90294c063abbd244c5ad19a384a1baf20aafd43a
nguyenthininh <45448845+nguyenthininh@users.noreply.github.com>
2020-03-02 01:35:31 +0700

    demo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,80 +2,14 @@
 @section('title',"Trang chủ")
 @section('home')
 
-    <div class="row align-items-center">
-        <div class="col-lg-12">
-            <div class="banner_slider owl-carousel">
-                <div class="single_banner_slider">
-                    <div class="row">
-                        <div class="col-lg-5 col-md-8">
-                            <div class="banner_text">
-                                <div class="banner_text_iner">
-                                    <h1>Wood & Cloth
-                                        Sofa</h1>
-                                    <p>Incididunt ut labore et dolore magna aliqua quis ipsum
-                                        suspendisse ultrices gravida. Risus commodo viverra</p>
-                                    <a href="#" class="btn_2">buy now</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="banner_img d-none d-lg-block">
-                            <img src="img/banner_img.png" alt="">
-                        </div>
-                    </div>
-                </div><div class="single_banner_slider">
-                    <div class="row">
-                        <div class="col-lg-5 col-md-8">
-                            <div class="banner_text">
-                                <div class="banner_text_iner">
-                                    <h1>Cloth & Wood
-                                        Sofa</h1>
-                                    <p>Incididunt ut labore et dolore magna aliqua quis ipsum
-                                        suspendisse ultrices gravida. Risus commodo viverra</p>
-                                    <a href="#" class="btn_2">buy now</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="banner_img d-none d-lg-block">
-                            <img src="img/banner_img.png" alt="">
-                        </div>
-                    </div>
-                </div><div class="single_banner_slider">
-                    <div class="row">
-                        <div class="col-lg-5 col-md-8">
-                            <div class="banner_text">
-                                <div class="banner_text_iner">
-                                    <h1>Wood & Cloth
-                                        Sofa</h1>
-                                    <p>Incididunt ut labore et dolore magna aliqua quis ipsum
-                                        suspendisse ultrices gravida. Risus commodo viverra</p>
-                                    <a href="#" class="btn_2">buy now</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="banner_img d-none d-lg-block">
-                            <img src="img/banner_img.png" alt="">
-                        </div>
-                    </div>
+    <div class="row justify-content-center">
+        <div class="col-lg-8">
+            <div class="breadcrumb_iner">
+                <div class="breadcrumb_iner_item">
+                    <h2>Shop Single</h2>
+                    <p>Home <span>-</span> Shop Single</p>
                 </div>
-                <!-- <div class="single_banner_slider">
-                    <div class="row">
-                        <div class="col-lg-5 col-md-8">
-                            <div class="banner_text">
-                                <div class="banner_text_iner">
-                                    <h1>Cloth $ Wood Sofa</h1>
-                                    <p>Incididunt ut labore et dolore magna aliqua quis ipsum
-                                        suspendisse ultrices gravida. Risus commodo viverra</p>
-                                    <a href="#" class="btn_2">buy now</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="banner_img d-none d-lg-block">
-                            <img src="img/banner_img.png" alt="">
-                        </div>
-                    </div>
-                </div> -->
             </div>
-            <div class="slider-counter"></div>
         </div>
     </div>
 
